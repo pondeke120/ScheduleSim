882ff823ce1a5d453a7f6c1dc34aac8051d40af2
pondeke120 <pondekewa@outlook.jp>
2018-12-23 02:16:24 +0900

    PertPageの見た目を少し変更

---
+++ ScheduleSim/ControlConverters/PertPageCriticalFlagConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ScheduleSim.ControlConverters
+{
+    public class PertPageCriticalFlagConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var flag = (bool)value;
+
+            return
+                flag ? "○" : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
