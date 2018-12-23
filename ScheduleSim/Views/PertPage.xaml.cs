71a88c006297d9abc63cbc09125000054b79087c
pondeke120 <pondekewa@outlook.jp>
2018-12-23 15:07:09 +0900

    PertPageの列ヘッダを固定

--- ScheduleSim/Views/PertPage.xaml.cs
+++ ScheduleSim/Views/PertPage.xaml.cs
@@ -84,5 +84,21 @@ namespace ScheduleSim.Views
 
             cb.ItemsSource = source;
         }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            // オーバーフローパネルを非表示にする
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+            var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+            {
+                mainPanelBorder.Margin = new Thickness();
+            }
+        }
     }
 }
