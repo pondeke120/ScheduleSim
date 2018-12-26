ba51737c3efba769bc43b8f6101c902c14dc8898
pondeke120 <pondekewa@outlook.jp>
2018-12-26 21:52:20 +0900

    ProcessDependencyPageの行削除を複数選択可能に変更

--- ScheduleSim/Commands/ProcessDependencyPage/DeleteDependencyCommand.cs
+++ ScheduleSim/Commands/ProcessDependencyPage/DeleteDependencyCommand.cs
@@ -5,12 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using ScheduleSim.Core.Extensions;
+using System.Collections;
+using ScheduleSim.ViewModels;
+using ScheduleSim.Core.Contexts;
 
 namespace ScheduleSim.Commands.ProcessDependencyPage
 {
     public class DeleteDependencyCommand : ICommand
     {
+        private AppContext appContext;
+
         public event EventHandler CanExecuteChanged;
+
+        public DeleteDependencyCommand(
+            AppContext appContext)
+        {
+            this.appContext = appContext;
+        }
 
         public bool CanExecute(object parameter)
         {
@@ -19,7 +31,14 @@ namespace ScheduleSim.Commands.ProcessDependencyPage
 
         public void Execute(object parameter)
         {
-            MessageBox.Show("delete");
+            var viewModels = (parameter as IList).Cast<object>()
+                .Where(x => x is ProcessDependencyPageDependencyItemViewModel)
+                .Cast<ProcessDependencyPageDependencyItemViewModel>().ToArray();
+            var removeDependencies = this.appContext.ProcessDependencies.Where(x => viewModels.Any(y => y.SrcProcessId == x.OrgProcessCd && y.DstProcessId == x.DstProcessCd)).ToArray();
+            if (removeDependencies.Length > 0)
+            {
+                this.appContext.ProcessDependencies.RemoveRange(removeDependencies);
+            }
         }
     }
 }
