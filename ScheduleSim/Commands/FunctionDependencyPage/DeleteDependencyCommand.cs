e2f70a6fe085b039d08e1c2e9e808be26624dc1e
pondeke120 <pondekewa@outlook.jp>
2018-12-26 22:10:36 +0900

    FunctionDependencyPageの行削除を複数選択可能に変更

--- ScheduleSim/Commands/FunctionDependencyPage/DeleteDependencyCommand.cs
+++ ScheduleSim/Commands/FunctionDependencyPage/DeleteDependencyCommand.cs
@@ -5,12 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using ScheduleSim.Core.Extensions;
+using System.Collections;
+using ScheduleSim.ViewModels;
+using ScheduleSim.Core.Contexts;
 
 namespace ScheduleSim.Commands.FunctionDependencyPage
 {
     public class DeleteDependencyCommand : ICommand
     {
+        private AppContext appContext;
+
         public event EventHandler CanExecuteChanged;
+
+        public DeleteDependencyCommand(
+            AppContext appContext)
+        {
+            this.appContext = appContext;
+        }
 
         public bool CanExecute(object parameter)
         {
@@ -19,7 +31,14 @@ namespace ScheduleSim.Commands.FunctionDependencyPage
 
         public void Execute(object parameter)
         {
-            MessageBox.Show("delete");
+            var viewModels = (parameter as IList).Cast<object>()
+                .Where(x => x is FunctionDependencyPageDependencyItemViewModel)
+                .Cast<FunctionDependencyPageDependencyItemViewModel>().ToArray();
+            var removeDependencies = this.appContext.FunctionDependencies.Where(x => viewModels.Any(y => y.SrcFunctionId == x.OrgFunctionCd && y.DstFunctionId == x.DstFunctionCd)).ToArray();
+            if (removeDependencies.Length > 0)
+            {
+                this.appContext.FunctionDependencies.RemoveRange(removeDependencies);
+            }
         }
     }
 }
